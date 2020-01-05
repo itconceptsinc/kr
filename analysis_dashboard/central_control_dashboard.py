5e80cc92cdb770eef20d796eaa002d91fd558aae
Joel Klein <joel.klein@useitc.com>
2020-01-05 01:25:19 -0500

    Added initial dasboard for tracking train circuit positions

---
+++ analysis_dashboard/central_control_dashboard.py
@@ -0,0 +1,78 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table
+
+from dash.dependencies import Output, Input
+
+from config import DEBUG
+from utils.wmata_static import get_circuit_ids
+from stream_analysis.train_pos_rrcf import TrainPosRRCF
+
+circuits = get_circuit_ids()
+line_colors = {'BL': 'blue', 'GR': 'green', 'OR': 'orange', 'RD': 'red', 'SV': 'grey', 'YL': 'yellow'}
+train_pos_rrcf = TrainPosRRCF()
+
+if DEBUG:
+    train_pos_rrcf.process_msgs(350)
+
+columns = ['cars', 'direction', 'circuit', 'seconds_at_loc', 'anomaly_score']
+tbl_cols = [{"name": i, "id": i} for i in columns]
+
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div()
+
+app.layout = html.Div(id='table-wrapper',
+                      style={'width': 'auto', 'overflow-y': 'scroll','max-width': '90vw', 'margin':'0 auto 50px'},
+                      children=[
+                          dash_table.DataTable(id='live-table',
+                                               data=[{}],
+                                               columns=tbl_cols,
+                                               style_cell={'textAlign': 'center','min-width':'50px'},
+                                               filter_action="native",
+                                               sort_action="native",
+                                               # sort_mode="multi",
+                                               column_selectable="single",
+                                               row_selectable="multi",
+                                               # row_deletable=True,
+                                               selected_columns=[],
+                                               selected_rows=[],
+                                               page_action="native",
+                                               page_current=0,
+                                               page_size=15
+                                               ),
+                          dcc.Interval(
+                              id='graph-update',
+                              interval=60 * 100,
+                              n_intervals=0
+                          ),
+                      ]
+                      )
+
+# app.layout = html.Div(
+#     [
+#         dash_table.DataTable(id='live-table',
+#                       data=[],
+#                       columns=columns,
+#                       style_cell={'textAlign': 'center', 'min-width': '50px'},
+#                       )
+#         dcc.Interval(
+#             id='graph-update',
+#             interval=60*1000,
+#             n_intervals=0
+#         ),
+#     ]
+# )
+
+@app.callback(Output('live-table', 'data'),
+              [Input('graph-update', 'n_intervals')])
+def update_graph_scatter(n_interval):
+    train_pos_rrcf.process_msgs(1)
+    scores = train_pos_rrcf.get_flattened_last_scores()
+
+    return scores
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
