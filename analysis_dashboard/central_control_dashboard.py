ff38c57821f01ec240ef7ee88ec206729ded8836
Joel Klein <joel.klein@useitc.com>
2020-01-05 18:36:51 -0500

    Updates to dashboard to incorporate gtfs consumer

--- analysis_dashboard/central_control_dashboard.py
+++ analysis_dashboard/central_control_dashboard.py
@@ -16,14 +16,17 @@ except:
 
 from config import DEBUG
 from utils.wmata_static import get_circuit_ids
-from stream_analysis.train_pos_rrcf import TrainPosRRCF
+from stream_analysis.train_pos_consumer import TrainPosRRCF
+from stream_analysis.train_gtfs_consumer import TrainGTFS
 
 circuits = get_circuit_ids()
 line_colors = {'BL': 'blue', 'GR': 'green', 'OR': 'orange', 'RD': 'red', 'SV': 'grey', 'YL': 'yellow'}
 train_pos_rrcf = TrainPosRRCF()
+train_gtfs = TrainGTFS()
 
 if DEBUG:
-    train_pos_rrcf.process_msgs(150)
+    train_pos_rrcf.process_msgs(100)
+    train_gtfs.process_msgs(1)
 
 columns = ['cars', 'direction', 'circuit', 'seconds_at_loc', 'anomaly_score']
 tbl_cols = [{"name": i, "id": i} for i in columns]
@@ -41,33 +44,59 @@ app = dash.Dash(__name__)
 
 app.layout = html.Div()
 
-app.layout = html.Div(id='table-wrapper',
-                      style={'width': 'auto', 'overflow-y': 'scroll','max-width': '90vw', 'margin':'0 auto 50px'},
-                      children=[
-                          dash_table.DataTable(id='live-table',
-                                               data=[{}],
-                                               columns=tbl_cols,
-                                               style_cell={'textAlign': 'center','min-width':'50px'},
-                                               filter_action="native",
-                                               sort_action="native",
-                                               # sort_mode="multi",
-                                               column_selectable="single",
-                                               row_selectable="multi",
-                                               # row_deletable=True,
-                                               selected_columns=[],
-                                               selected_rows=[],
-                                               page_action="native",
-                                               page_current=0,
-                                               page_size=15,
-                                               style_data_conditional=styles,
-                                               ),
-                          dcc.Interval(
-                              id='graph-update',
-                              interval=60 * 100,
-                              n_intervals=0
-                          ),
-                      ]
-                      )
+app.layout = html.Div([
+    html.Div(id='table-wrapper',
+             style={'width': 'auto', 'overflow-y': 'scroll','max-width': '90vw', 'margin':'0 auto 50px'},
+             children=[
+                 # Circuit Anomaly Table
+                 dash_table.DataTable(id='circuit_anomaly_table',
+                                      data=[{}],
+                                      columns=tbl_cols,
+                                      style_cell={'textAlign': 'center','min-width':'50px'},
+                                      filter_action="native",
+                                      sort_action="native",
+                                      # sort_mode="multi",
+                                      column_selectable="single",
+                                      row_selectable="multi",
+                                      # row_deletable=True,
+                                      selected_columns=[],
+                                      selected_rows=[],
+                                      page_action="native",
+                                      page_current=0,
+                                      page_size=15,
+                                      style_data_conditional=styles
+                                      ),
+                 dcc.Interval(
+                     id='graph-update',
+                     interval=60 * 100,
+                     n_intervals=0
+                 ),
+             ]),
+
+    html.Div(id='table-wrapper2',
+             style={'width': 'auto', 'overflow-y': 'scroll','max-width': '90vw', 'margin':'0 auto 50px'},
+             children=[
+                 # GTFS Table
+                 dash_table.DataTable(id='gtfs-table',
+                                      data=[{}],
+                                      columns=[],
+                                      style_cell={'textAlign': 'center','min-width':'50px'},
+                                      filter_action="native",
+                                      sort_action="native",
+                                      # sort_mode="multi",
+                                      column_selectable="single",
+                                      # row_deletable=True,
+                                      page_action="native",
+                                      page_current=0,
+                                      page_size=15,
+                                      ),
+                 dcc.Interval(
+                     id='gtfs-update',
+                     interval=60 * 1000,
+                     n_intervals=0
+                 ),
+             ])
+    ])
 
 # app.layout = html.Div(
 #     [
@@ -84,13 +113,24 @@ app.layout = html.Div(id='table-wrapper',
 #     ]
 # )
 
-@app.callback(Output('live-table', 'data'),
+@app.callback(Output('circuit_anomaly_table', 'data'),
               [Input('graph-update', 'n_intervals')])
-def update_graph_scatter(n_interval):
+def update_circuit_anomalies_table(n_interval):
     train_pos_rrcf.process_msgs(1)
     scores = train_pos_rrcf.get_flattened_last_scores()
 
     return scores
+
+@app.callback([Output('gtfs-table', 'data'),
+               Output('gtfs-table', 'columns')],
+              [Input('gtfs-update', 'n_intervals')])
+def update_gtfs_table(n_interval):
+    train_gtfs.process_msgs(1)
+    df = train_gtfs.get_past_data()[0]
+    cols = [{'name': col, 'id': col} for col in df.columns]
+    data = df.to_dict('rows')
+    return data, cols
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
